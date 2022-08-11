fef04a16bb6735cc572d3ca47c1677c4ff02dea6
Moritz Halbritter <mkammerer@vmware.com>
2022-08-11 15:14:01 +0200

    GraalVM bug report

--- session-redis-webflux/src/main/java/com/example/session/redis/webflux/SessionRedisWebfluxApplication.java
+++ session-redis-webflux/src/main/java/com/example/session/redis/webflux/SessionRedisWebfluxApplication.java
@@ -1,13 +1,20 @@
 package com.example.session.redis.webflux;
 
 import org.springframework.aot.smoketest.thirdpartyhints.NettyRuntimeHints;
-import org.springframework.aot.smoketest.thirdpartyhints.ReactorNettyHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
+// JVM: 33
+// Native image with lettuce & without hints: 0 (THIS IS WRONG, means all are skippable)
+// Native image with lettuce & with hints: 33 (Correct, channelRead and exceptionCaught are skippable)
+// Native image without lettuce & without hints: 511 (OK, no method is skippable - happens because reflection metadata is missing)
+// Native image without lettuce & with hints: 33 (Correct, see above)
+
 @SpringBootApplication
-@ImportRuntimeHints({ NettyRuntimeHints.class, ReactorNettyHints.class })
+// Uncomment for hints on reactor.netty.transport.ServerTransport$Acceptor
+// @ImportRuntimeHints({ NettyRuntimeHints.class, ReactorNettyHints.class })
+@ImportRuntimeHints(NettyRuntimeHints.class)
 public class SessionRedisWebfluxApplication {
 
 	public static void main(String[] args) throws InterruptedException {
